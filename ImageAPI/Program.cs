aa630be2dcacbee48a7e46585308ef3dccaa4a87
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-12-17 01:22:22 +0100

    ændret using ImageAPI.Repository;

--- ImageAPI/Program.cs
+++ ImageAPI/Program.cs
@@ -1,36 +1,46 @@
 using Microsoft.EntityFrameworkCore;
 using ImageAPI;
+using ImageAPI.Repository; 
 
 var builder = WebApplication.CreateBuilder(args);
 
+// CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "AllowAll",
                       policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 });
 
+// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Controllers
 builder.Services.AddControllers();
+
+// Database context
 builder.Services.AddDbContext<ImageDbContext>(options =>
 {
     options.UseSqlServer(Secret.ConnectionString);
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Registrér ImageRepositoryDB som en service
+builder.Services.AddScoped<ImageRepositoryDB>();
 
 var app = builder.Build();
 
+// Initialize database
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ImageDbContext>();
     db.Database.EnsureCreated(); // Initialize the database
 }
 
+// Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
+
+// CORS policy
 app.UseCors("AllowAll");
 
 app.UseAuthorization();
